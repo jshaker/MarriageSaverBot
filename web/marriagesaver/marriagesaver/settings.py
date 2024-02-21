ed00bac6a365fb9347e40ec42c7828c333d4663a
Jeremie Shaker <jeremie.shaker@hotmail.com>
2024-02-21 14:07:47 -0800

    add wildcard allowed_hosts

--- web/marriagesaver/marriagesaver/settings.py
+++ web/marriagesaver/marriagesaver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@3$#6kg%h8eiv8nn0gallsju8m(sma-u^)v88+29a%i@*^h-4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
